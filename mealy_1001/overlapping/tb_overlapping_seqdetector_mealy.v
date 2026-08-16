b93399cf2d20519fec83ee5065be954c8f29669c
pranavnandakumar-techie <pranavntechie@gmail.com>
2026-08-16 09:13:57 +0530

    Create testbench for overlapping_seqdetector_mealy
    Added a testbench for the overlapping Mealy sequence detector.

--- mealy_1001/overlapping/tb_overlapping_seqdetector_mealy.v
+++ mealy_1001/overlapping/tb_overlapping_seqdetector_mealy.v
@@ -1,1 +1,33 @@
+`timescale 1ns / 1ps
+module tb_overlapping_seqdetector_mealy();
+    reg x,CLK;
+    wire y;
+    overlapping_seqdetector_mealy dut(x,CLK,y);
+    initial begin 
+        CLK=0;
+        forever #5 CLK=~CLK;
+    end
+    initial begin
+        x=1;#5;
+        x=1;#10;
+        x=0;#10;
+        x=0;#10;
+        x=1;#10;
+        x=0;#10;
+        x=0;#10;
+        x=1;#10;
+        x=0;#10;
+        x=0;#10;
+        x=1;#10;
+        x=0;#10;
+        x=0;#10;
+        x=0;#10;
+        x=0;#10;
+        x=1;#10;
+        x=0;#10;
+        x=0;#10;
+        x=1;#10;
+        $finish;
+    end
+endmodule
 
