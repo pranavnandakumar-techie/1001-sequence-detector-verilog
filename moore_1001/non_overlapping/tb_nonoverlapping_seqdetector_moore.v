64722d3826e3623619d073ae3fa967978baff643
pranavnandakumar-techie <pranavntechie@gmail.com>
2026-08-16 09:40:48 +0530

    Add testbench for non-overlapping sequence detector

--- moore_1001/non_overlapping/tb_nonoverlapping_seqdetector_moore.v
+++ moore_1001/non_overlapping/tb_nonoverlapping_seqdetector_moore.v
@@ -1,1 +1,30 @@
+`timescale 1ns / 1ps
+module tb_nonoverlapping_seqdetector_moore();
+    reg x,CLK;
+    wire y;
+    non_overlapping_seqdetector_moore dut(x,CLK,y);
+    initial begin
+        CLK=0;
+        forever #5 CLK=~CLK;
+    end
+    initial begin
+       x=1;#15;
+       x=0;#10;
+       x=0;#10;
+       x=1;#10;
+       x=0;#10;
+       x=0;#10;
+       x=1;#10;
+       x=0;#10;
+       x=0;#10;
+       x=0;#10;
+       x=0;#10;
+       x=0;#10;
+       x=1;#10;
+       x=0;#10;
+       x=0;#10;
+       x=1;#10;
+       $finish;
+    end
+endmodule
 
