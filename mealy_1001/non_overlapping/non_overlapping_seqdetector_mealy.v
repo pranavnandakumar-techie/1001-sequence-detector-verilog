2d61f0469a761f314f4840080a59d3bb4ab60321
pranavnandakumar-techie <pranavntechie@gmail.com>
2026-08-16 09:19:56 +0530

    Add non-overlapping Mealy sequence detector module

--- mealy_1001/non_overlapping/non_overlapping_seqdetector_mealy.v
+++ mealy_1001/non_overlapping/non_overlapping_seqdetector_mealy.v
@@ -1,1 +1,24 @@
+`timescale 1ns / 1ps
+module d_flip(D,CLK,Q,Qd);
+    input D,CLK;
+    output reg Q,Qd;
+    initial begin
+        Q=0;
+        Qd=1;
+    end
+    always@(negedge CLK) begin
+        Q=D;
+        Qd=~D;
+    end
+endmodule
+module non_overlapping_seqdetector_mealy(x,CLK,y);
+    input x,CLK;
+    output y;
+    wire q1,q0,d1,d0,qd0,qd1;
+    assign d1=((~x)&(~q1)&q0) | ((~x)&q1&(~q0));
+    assign d0=(x&(~q1)) | (q1&(~q0));
+    assign y=x&q1&q0;
+    d_flip FF0(d0,CLK,q0,qd0);
+    d_flip FF1(d1,CLK,q1,qd1);
+endmodule
 
