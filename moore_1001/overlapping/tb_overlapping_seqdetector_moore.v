5a5d3b70e46e1ae1cb83160cb50582b6c04ce615
pranavnandakumar-techie <pranavntechie@gmail.com>
2026-08-16 09:30:43 +0530

    Create testbench for overlapping_seqdetector_moore
    Added testbench for overlapping sequence detector in Moore state machine.

--- moore_1001/overlapping/tb_overlapping_seqdetector_moore.v
+++ moore_1001/overlapping/tb_overlapping_seqdetector_moore.v
@@ -1,1 +1,30 @@
+`timescale 1ns / 1ps
+module tb_overlapping_seqdetector_moore();
+    reg x,CLK;
+    wire y;
+    overlapping_seqdetector_moore dut(x,CLK,y);
+    initial begin
+        CLK=0;
+        forever #5 CLK=~CLK;
+    end
+    initial begin
+       x=1;#5;
+       x=0;#10;
+       x=0;#10;
+       x=1;#10;
+       x=0;#10;
+       x=0;#10;
+       x=1;#10;
+       x=0;#10;
+       x=0;#10;
+       x=0;#10;
+       x=0;#10;
+       x=0;#10;
+       x=1;#10;
+       x=0;#10;
+       x=0;#10;
+       x=1;#10;
+       $finish;
+    end
+endmodule
 
